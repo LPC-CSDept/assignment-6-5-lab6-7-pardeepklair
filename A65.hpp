d9d1f833db4eed8dbc18255d08920621a6cbd701
Pardeep Klair <106414050+pardeepklair@users.noreply.github.com>
2022-10-09 00:19:09 -0700

    Add files via upload

--- A65.hpp
+++ A65.hpp
@@ -9,3 +9,23 @@ using namespace std;
 //              This function will use swaptwo().
 
 // swaptwo() ; to swap two values (a, b) to (b, a)
+
+
+void getinput(int &n1, int &n2, int &n3) {
+    cout << "Enter Number 1: " << endl;
+    cin >> n1;
+    cout << "Enter Number 2: " << endl;
+    cin >> n2;
+    cout << "Enter Number 3: " << endl;
+    cin >> n3;   
+
+}
+
+void swap(int &n1, int &n2, int &n3) {
+    n1 = n1 + n2 + n3;
+
+    n2 = n1 - (n2+n3);
+    n3 = n1 - (n2+n3);
+    n1 = n1 - (n2+n3);   
+
+}
